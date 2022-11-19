d9d28bdef097fef2f55eeefb2a6816985ef51ddd
Thiago Mussolini <mussolinithiago1994@gmail.com>
2022-11-18 22:17:35 -0300

    Realizado o relacionamento entre as tabelas

--- documentos/models.py
+++ documentos/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from funcionarios.models import Funcionario
 
 class Documentos(models.Model):
     descricao = models.CharField(max_length=100)
+    pertence = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.descricao
