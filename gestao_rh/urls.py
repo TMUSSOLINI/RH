5ea541848bdd2dbb5df22f314545d04e1e164720
Thiago Mussolini <mussolinithiago1994@gmail.com>
2022-11-19 18:28:51 -0300

    CRUD de empresas

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('funcionarios/', include('funcionarios.urls')),
+    path('empresas/', include('empresas.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
