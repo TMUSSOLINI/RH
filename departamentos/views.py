25748a50797fec348aeb02698485c28fe729b3c6
Thiago Mussolini <mussolinithiago1994@gmail.com>
2022-11-24 12:40:58 -0300

    Feito o create

--- departamentos/views.py
+++ departamentos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Departamentos
 
 
@@ -9,3 +9,13 @@ class DepartamentosList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return Departamentos.objects.filter(empresa=empresa_logada)
+
+class DepartamentoCreate(CreateView):
+    model = Departamentos
+    fields = ['nome', ]
+
+    def form_valid(self, form):
+        departamento = form.save(commit=False)
+        departamento.empresa = self.request.user.funcionario.empresa
+        departamento.save()
+        return super(DepartamentoCreate, self).form_valid(form)
