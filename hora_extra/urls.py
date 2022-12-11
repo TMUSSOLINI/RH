f907e4d67a930122f20f92374ac46214ac127316
Thiago Mussolini <mussolinithiago1994@gmail.com>
2022-12-11 18:36:17 -0300

    Finalizado parte de relatorios

--- hora_extra/urls.py
+++ hora_extra/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import HoraExtraList, HoraExtraEdit, HoraExtraDelete, HoraExtraNovo, UtilizouHoraExtra
+from .views import ( HoraExtraList, HoraExtraEdit, HoraExtraDelete,HoraExtraNovo,
+                     UtilizouHoraExtra,ExportarParaCsv, ExportarExcel)
 
 
 urlpatterns = [
@@ -8,4 +9,6 @@ urlpatterns = [
     path('editar/<int:pk>/', HoraExtraEdit.as_view(), name='update_hora_extra'),
     path('utilizou-hora-extra/<int:pk>/', UtilizouHoraExtra.as_view(), name='utilizou-hora-extra'),
     path('deletar/<int:pk>/', HoraExtraDelete.as_view(), name='delete_hora_extra'),
+    path('exportar-csv', ExportarParaCsv.as_view(), name='exportar_csv'),
+    path('exportar-excel', ExportarExcel.as_view(), name='exportar_excel'),
 ]
--- hora_extra/views.py
+++ hora_extra/views.py
@@ -1,9 +1,11 @@
+import csv
 import json
 
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import View
+import xlwt
 
 from .forms import RegistroHoraExtraForm
 from django.views.generic import ListView, UpdateView, DeleteView, CreateView
@@ -50,3 +52,53 @@ class UtilizouHoraExtra(View):
         registro_hora_extra.utilizada = True
         registro_hora_extra.save()
         return HttpResponse(response, content_type='application/json')
+
+
+class ExportarParaCsv(View):
+    def get(self, request):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="somefile.csv"'
+
+        registro_he = HoraExtra.objects.filter(utilizado=False)
+
+        writer = csv.writer(response)
+        writer.writerow(['ID', 'Motivo', 'Funcionario', 'Horas'])
+
+        for registro in registro_he:
+            writer.writerow([registro.id, registro.motivo,
+                             registro.funcionario, registro.horas])
+
+        return response
+
+class ExportarExcel(View):
+    def get(self, request):
+        response = HttpResponse(content_type='application/ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="users.xls"'
+
+        wb = xlwt.Workbook(encoding='urf-8')
+        ws = wb.add_sheet('Users')
+
+        row_num = 0
+
+        font_style = xlwt.XFStyle()
+        font_style.font.bold= True
+
+        columns = ['ID', 'Motivo', 'Funcionario', 'Horas']
+
+        for col_num in range(len(columns)):
+            ws.write(row_num, col_num, columns[col_num], font_style)
+
+        font_style = xlwt.XFStyle()
+
+        registros = HoraExtra.objects.filter(utilizado=False)
+
+        row_num = 1
+        for registro in registros:
+            ws.write(row_num, 0, registro.id, font_style)
+            ws.write(row_num, 1, registro.motivo, font_style)
+            ws.write(row_num, 2, registro.funcionario.nome, font_style)
+            ws.write(row_num, 3, registro.horas, font_style)
+            row_num+= 1
+
+        wb.save(response)
+        return  response
